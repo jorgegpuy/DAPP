a75f13b4484a7ee5aa138899707d53f95d3b3d63
Christianpereira85 <38117481+Christianpereira85@users.noreply.github.com>
2020-02-28 11:44:55 -0300

    login

---
+++ login.py
@@ -0,0 +1,22 @@
+
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+]
+{% extends 'base.html' %}
+
+{% block title %}Login{% endblock %}
+
+{% block content %}
+  <h2>Login</h2>
+  <form method="post">
+    {% csrf_token %}
+    {{ form.as_p }}
+    <button type="submit">Login</button>
+  </form>
+{% endblock %}
